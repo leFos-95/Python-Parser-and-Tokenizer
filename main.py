c5f3e239ca1caff0f24af2a3ce86b002350780cd
leFos-95 <lefteris95.soulis@gmail.com>
2021-12-03 17:41:22 +0200

    Retrieve identifiers

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ def tokenize_text(file_path):
     tokenized_text = scanner.scan(raw_text)
     return tokenized_text
 
+def retrieve_file_Java_identifiers(tokenized_text):
+    file_identifier_list = []
+    for keyword in tokenized_text[0]:
+        if keyword[1] == "identifier":
+            file_identifier_list.append(keyword)
+    return file_identifier_list
+
 
 print(tokenize_text('java.txt'))
+text_in_tokens = tokenize_text('java.txt')
+print(retrieve_file_Java_identifiers(text_in_tokens))
