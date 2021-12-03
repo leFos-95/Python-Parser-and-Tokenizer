6a3f1b5ca5043f93fde14bac710d425073c935a6
leFos-95 <lefteris95.soulis@gmail.com>
2021-12-03 18:05:48 +0200

    Retrieve comment tags

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ def retrieve_file_Java_identifiers(tokenized_text):
             file_identifier_list.append(keyword)
     return file_identifier_list
 
+def retrieve_file_Java_comment_tags(tokenized_text):
+    file_comment_list = []
+    for comment_index, keyword in enumerate(tokenized_text[0]):
+        if(keyword[1] == "comment"):
+            file_comment_list.extend([(keyword, comment_index)])
+    return file_comment_list
+
 
 print(tokenize_text('java.txt'))
 text_in_tokens = tokenize_text('java.txt')
 print(retrieve_file_Java_identifiers(text_in_tokens))
+token_map = retrieve_file_Java_comment_tags(text_in_tokens)
+print(token_map)
